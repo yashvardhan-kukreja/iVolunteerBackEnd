85a3b5ce55df91309585e7df529b2a2b382422de
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2017-11-11 02:35:56 +0530

    Added the mlab url

--- database/config.js
+++ database/config.js
@@ -2,6 +2,6 @@
  * Created by Yash 1300 on 14-09-2017.
  */
 module.exports = {
-    database:"mongodb://localhost:27017/ivolunteer",
+    database:"mongodb://yashvardhan:yash.kukreja.98@gmail.com@ds155315.mlab.com:55315/ivolunteer",
     secretkey: "luci4"
 };
