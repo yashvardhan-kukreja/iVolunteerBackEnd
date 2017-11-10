96e103460d107634fcb85af6b9047d144e394edd
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2017-11-11 03:01:38 +0530

    final commit

--- database/config.js
+++ database/config.js
@@ -2,6 +2,6 @@
  * Created by Yash 1300 on 14-09-2017.
  */
 module.exports = {
-    database:"mongodb://yashvardhan:yash.kukreja.98@gmail.com@ds155315.mlab.com:55315/ivolunteer",
+    database:"mongodb://yashvardhan:yashvardhan@ds155315.mlab.com:55315/ivolunteer",
     secretkey: "luci4"
 };
